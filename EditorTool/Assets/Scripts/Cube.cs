7bafa6223a5533bf4aa2be2404696599858b07ae
Nicholas Trevino <nicholasltrev@gmail.com>
2020-12-03 22:18:10 -0600

    Initial change

---
+++ EditorTool/Assets/Scripts/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    Color color;
+
+    private void Awake()
+    {
+        color = GetComponent<Renderer>().sharedMaterial.color;
+    }
+
+    public void GenerateColor()
+    {
+        GetComponent<Renderer>().sharedMaterial.color = Random.ColorHSV();
+    }
+
+    public void Reset()
+    {
+        GetComponent<Renderer>().sharedMaterial.color = color;
+    }
+}
